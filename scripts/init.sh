a755c8e113a5b7f15a453c51d46d3218e1c3e688
Peter Johnson <petejohnson84@users.noreply.github.com>
2018-01-13 11:19:29 -0700

    Increase wordpress file size limit to 8M

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,3 +13,7 @@ cd /var/www/public
 wp core download
 wp core config --dbhost=localhost --dbname=wp --dbuser=root --dbpass=root
 wp core install --url=192.168.33.10 --title="WordPress Sandbox" --admin_name=admin --admin_password=@dm1n! --admin_email=me@wp.sandbox
+
+# Change php.ini upload_max_filesize
+sudo sed -i.bak "s/upload_max_filesize = 2M/upload_max_filesize = 8M/" /etc/php/7.0/apache2/php.ini
+sudo /etc/init.d/apache2 restart
