956a2efe951e23b19bf2be3e1c52968703202e1b
Peter Johnson <petejohnson84@users.noreply.github.com>
2018-01-14 10:41:54 -0700

    Migration functionality

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,5 +1,5 @@
 # Custom bash commands go here.
 
 #!/bin/bash
-cd /var/www/public/
-ls -1 wp-content/ai1wm-backups/*.wpress 2>/dev/null | xargs --no-run-if-empty wp ai1wm restore
+cd /var/www/public/wp-content/ai1wm-backups
+ls -1 *.wpress 2>/dev/null | xargs --no-run-if-empty wp ai1wm restore
